3c3765f90073eb44ec007e798d90642d6c6932e7
ehilel <ehilel@g.jct.ac.il>
2024-11-06 09:37:01 +0200

    del config

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  env: {
-    PUBLIC_DB_CONNECTION: "mongodb+srv://efrat10770:NjTHcueeXqvv4ufa@cluster0.jfuxp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-  }
-};
-
-export default nextConfig;
